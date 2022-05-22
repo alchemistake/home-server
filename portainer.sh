f66b4b1e45b8ba0309a6f8563d6e177931eb9b3d
Alchemistake <alchemistake@gmail.com>
2022-05-22 21:42:05 +0200

    Portainer

---
+++ portainer.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+docker volume create portainer_data && \
+docker run -d -p 80:8000 -p 443:9443 --name portainer \
+    --restart=always \
+    --privileged \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v portainer_data:/data \
+    portainer/portainer-ce:2.9.3
