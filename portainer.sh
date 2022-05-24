f8e16fac71562b2f080913f2753372eac1bc3a2b
Alchemistake <alchemistake@gmail.com>
2022-05-24 15:13:54 +0200

    Portainer Latest

--- portainer.sh
+++ portainer.sh
@@ -5,4 +5,4 @@ docker run -d -p 80:8000 -p 443:9443 --name portainer \
     --privileged \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v portainer_data:/data \
-    portainer/portainer-ce:2.9.3
+    portainer/portainer-ce:latest
