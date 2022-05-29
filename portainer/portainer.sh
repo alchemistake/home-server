f1cbb953ca19a3c52c27c684c5ed3c221e42659e
Caner Caliskaner <caner.caliskaner@cognite.com>
2022-05-29 17:36:14 +0200

    better traefik done for everyone

--- portainer/portainer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-docker volume create portainer_data && \
-docker run -d -p 80:8000 -p 443:9443 --name portainer \
-    --restart=always \
-    --privileged \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    -v portainer_data:/data \
-    portainer/portainer-ce:latest
